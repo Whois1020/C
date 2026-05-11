1e1d1148928a2f791dfd2ab4ba8a174f367913cf
Whois1020 <whoisgithun@gmail.com>
2026-05-11 03:55:06 -0500

    Add files via upload

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -14,9 +14,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     let fecha = fechaObj.toLocaleDateString('en-US', { timeZone: 'America/Lima' })
     let date = `${fecha} ${hora}`
     let mediaList = [
-      'https://qu.ax/SQIWW.mp4',
-      'https://qu.ax/CBbHZ.mp4', 
-      'https://qu.ax/vIKXn.mp4'
+      'https://o.uguu.se/MJVbgdQD.jpg'
     ]
 
     let banner = mediaList[Math.floor(Math.random() * mediaList.length)]
